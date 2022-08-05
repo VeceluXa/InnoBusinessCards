3cfe4e37d4f65d9534b196b58dd85e1afceb5879
Fyodor Danilov <danilovfa@protonmail.com>
2022-08-05 16:36:23 +0300

    Remove not used main fragment's features

--- app/src/main/java/com/danilovfa/innowisedatatransfer/MainActivity.kt
+++ app/src/main/java/com/danilovfa/innowisedatatransfer/MainActivity.kt
@@ -3,17 +3,27 @@ package com.danilovfa.innowisedatatransfer
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Toolbar
+import androidx.core.view.MenuProvider
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
+import com.danilovfa.innowisedatatransfer.databinding.ActivityMainBinding
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var toolbar: MaterialToolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        toolbar = binding.mainToolbar
+        setSupportActionBar(toolbar)
     }
 }
