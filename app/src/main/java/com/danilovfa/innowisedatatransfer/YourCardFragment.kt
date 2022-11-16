84fa7cf3f4d7369194e862a82c6e949798e6d5e0
Fyodor Danilov <danilovfa@protonmail.com>
2022-11-17 02:30:00 +0300

    Moved card view

--- app/src/main/java/com/danilovfa/innowisedatatransfer/YourCardFragment.kt
+++ app/src/main/java/com/danilovfa/innowisedatatransfer/YourCardFragment.kt
@@ -3,10 +3,10 @@ package com.danilovfa.innowisedatatransfer
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
-import androidx.appcompat.view.menu.MenuPopupHelper
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.lifecycle.Lifecycle
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.danilovfa.innowisedatatransfer.databinding.FragmentYourCardBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -19,6 +19,15 @@ class YourCardFragment : Fragment(), MenuProvider {
     private lateinit var binding: FragmentYourCardBinding
     private lateinit var auth: FirebaseAuth
 
+    private var isView = true
+
+    // Menu
+    private lateinit var menu: Menu
+    private lateinit var menuInflater: MenuInflater
+
+    // Nested nav controller
+    private lateinit var cardNavController: NavController
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -26,10 +35,11 @@ class YourCardFragment : Fragment(), MenuProvider {
         binding = FragmentYourCardBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        showNavBar()
+        // Save nested fragment state
+        if (savedInstanceState != null)
+            isView = savedInstanceState.getBoolean("IsView")
 
-        // Set title of tool bar
-        (requireActivity() as MainActivity).title = "Your card"
+        showNavBar()
 
         auth = Firebase.auth
 
@@ -44,21 +54,40 @@ class YourCardFragment : Fragment(), MenuProvider {
         val menuHost: MenuHost = requireActivity()
         menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
 
+        // Get nested nav controller
+        val nestedNavHostFragment = childFragmentManager.findFragmentById(R.id.cardContainer) as NavHostFragment
+        cardNavController = nestedNavHostFragment.navController
+
         // Inflate the layout for this fragment
         return view
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("IsView", isView)
+    }
+
     override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-        menuInflater.inflate(R.menu.toolbar_your_card_main, menu)
+        this.menu = menu
+        this.menuInflater = menuInflater
+        if (isView)
+            menuInflater.inflate(R.menu.toolbar_your_card_view, menu)
+        else
+            menuInflater.inflate(R.menu.toolbar_your_card_edit, menu)
     }
 
     // TODO Implement edit and share buttons
     override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
         return when (menuItem.itemId) {
             R.id.menu_your_card_edit -> {
+                viewToEdit()
                 true
             }
             R.id.menu_your_card_share -> {
+                true
+            }
+            R.id.menu_your_card_save -> {
+                editToView()
                 true
             }
             else -> false
@@ -70,5 +99,19 @@ class YourCardFragment : Fragment(), MenuProvider {
         val activity = requireActivity() as MainActivity
         val bottomNavigation = activity.findViewById<BottomNavigationView>(R.id.bottomNavigation)
         bottomNavigation.visibility = View.VISIBLE
+    }
+
+    private fun viewToEdit() {
+        isView = false
+        menu.clear()
+        menuInflater.inflate(R.menu.toolbar_your_card_edit, menu)
+        cardNavController.navigate(R.id.action_cardView_to_cardEdit)
+    }
+
+    private fun editToView() {
+        isView = true
+        menu.clear()
+        menuInflater.inflate(R.menu.toolbar_your_card_view, menu)
+        cardNavController.navigate(R.id.action_cardEdit_to_cardView)
     }
 }
